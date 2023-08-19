ccf4450b2f4a9e87c04cedc307efdb1d4bf7a8ea
Habibur Rahman <habib.swi16@gmail.com>
2023-08-20 01:47:43 +0600

    priec calculation

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
+let totalPrice = 0;
+
 function setInnerText(elementId, value) {
   element = document.getElementById(elementId);
   element.innerText = value;
 }
 
 function addToCart(target) {
-const cartContainer=document.getElementById("cart-container");
+  const cartContainer = document.getElementById("cart-container");
 
   const productName = target.childNodes[3].innerText.split("\n")[0];
 
   const li = document.createElement("li");
-  li.classList.add("text-[#111]", "text-2xl", "font-medium", "mt-4","list-decimal");
-  li.innerText=productName
+  li.classList.add(
+    "text-[#111]",
+    "text-2xl",
+    "font-medium",
+    "mt-4",
+    "list-decimal"
+  );
+  li.innerText = productName;
 
-  cartContainer.appendChild(li)
+  cartContainer.appendChild(li);
+
+  const productPrice = parseFloat(
+    target.childNodes[3].innerText.split("\n")[2]
+  );
+
+  totalPrice = totalPrice + productPrice;
+  setInnerText("total-price", totalPrice);
+
+ 
 }
